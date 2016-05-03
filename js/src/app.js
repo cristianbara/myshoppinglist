e2bad2a353a0fdc7bad96e7fc520aeb466b8b542
cristianbara <pathofexo@yahoo.com>
2016-05-03 16:58:54 +0200

    cleaned up code

--- js/src/app.js
+++ js/src/app.js
@@ -1,7 +1,16 @@
 /* 
  * (c) 2016 Cristian-Dan Bara, http://cristianbara.github.io/
  * License: CC BY-NC-ND 3.0, http://creativecommons.org/licenses/by-nc-nd/3.0/
-*/
+ */
+
+
+/*
+ * The starter model for the application. 
+ * It contains a set of list items, se second one is marked as done
+ * @property {object} model[@index {number}] 
+ * @property {string} model[@index {number}].text - the text information of each list item
+ * @property {boolean} model[@index {number}].done - marks wwther the item is done (true), or not (false)
+ */
 var model = [
     {
         text: 'Something you still have to buy',
@@ -14,6 +23,11 @@ var model = [
 
 ];
 
+/*
+ * Function which opens a url.
+ * @param {string} url - the URL string
+ * @return {undefined}
+ */
 var openURL = function (url) {
     if (device.platform === 'Android') {
         navigator.app.loadUrl(url, {
@@ -24,9 +38,14 @@ var openURL = function (url) {
     }
 };
 
+/*
+ * @module myToDoApp 
+ */
 angular.module('myToDoApp', ['contenteditable', 'ngStorage', 'ngAnimate'])
     .controller('myToDoAppController', function ($scope, $filter, $localStorage, $timeout) {
-        // controller code goes here
+        /*
+         * Loads data model from local storage is it exisits. 
+         */
 
         if ($localStorage.myToDos) {
 
@@ -35,82 +54,105 @@ angular.module('myToDoApp', ['contenteditable', 'ngStorage', 'ngAnimate'])
         } else {
 
             $scope.model = model;
-        }
+        };
 
+
+        /*
+         * orderBy is a function reference to the $filter service. 
+         */
         var orderBy = $filter('orderBy');
+
+
+        /*
+         * @param {boolean} $scope.showMenu - flag which trigger the display/hiding of the left hand side menu
+         */
         $scope.showMenu = false;
 
+
+
+        /*
+         * Function which orders a data @model according to the @done parameter and then save the result in the local storage.
+         * @return {undefined}
+         */
         $scope.orderModel = function () {
             $scope.model = orderBy($scope.model, 'done', false);
             // save the new $scope model to local storage
             $localStorage.myToDos = $scope.model;
-        }
+        };
+        
+        
 
+        /*
+         * Item functions
+         */
+        
+        /*
+         * Function which marks an item as done.
+         * @param {object} item - a @model item
+         * @return {undefined}
+         */
         $scope.markAsDone = function (item) {
             // change the done flag for the index item            
             item.done = true;
-
-            /* var tempItem = item;
-             
-             // remove item from the list
-             // take out the index item from the $scope model
-             for (i = 0; i < $scope.model.length; i++) {
-                 if ($scope.model[i] == item) {
-                     $scope.model.splice(i, 1);
-                 }
-             }
-             
-             // add tempItem to the bottom of the list      
-             
-             $timeout(function () {
-                 $scope.model.splice($scope.model.length, 0, tempItem);
-             }, 300);
-             
-             // save the new $scope model to local storage
-             $localStorage.myToDos = $scope.model;
-             */
+            // wait for 500ms, then order the model - move the done item in the lower part of the model
             $timeout(function () {
                 $scope.orderModel();
             }, 500);
 
-        }
+        };
 
+
+
+        /*
+         * Function which removes an item from the @model.
+         * @param {object} item - a @model item
+         * @return {undefined}
+         */
         $scope.removeToDo = function (item) {
             // take out the index item from the $scope model
             for (i = 0; i < $scope.model.length; i++) {
                 if ($scope.model[i] == item) {
                     $scope.model.splice(i, 1);
                 }
-            }
-
+            };
 
             // save the new $scope model to local storage
             $localStorage.myToDos = $scope.model;
 
-        }
+        };
 
+
+        /*
+         * Function which adds a default item to the @model.
+         * @return {undefined}
+         */
         $scope.addToDo = function () {
             // make a new, empty item
             var newItem = {
-                    text: '',
-                    done: false
-                }
-                // push the item to the $scope model list
-                //$scope.model.push(newItem);
+                text: '',
+                done: false
+            };
 
             // add new item to the top of the list
             $scope.model.splice(0, 0, newItem);
 
             // save the new $scope model to local storage
             $localStorage.myToDos = $scope.model;
-            
+
             // hide menu
             $scope.showMenu = false;
 
             //refresh $scope
             //$scope.$apply();
-        }
+        };
 
+
+
+        /*
+         * Function which marks an item as not done.
+         * @param {object} item - a @model item
+         * @return {undefined}
+         */
         $scope.markUndone = function (item) {
             item.done = false;
 
@@ -120,12 +162,30 @@ angular.module('myToDoApp', ['contenteditable', 'ngStorage', 'ngAnimate'])
             // save the new $scope model to local storage
             $localStorage.myToDos = $scope.model;
 
-        }
+        };
 
+
+
+        /*
+         * Function which toggles the left hand side menu.
+         * @return {undefined}
+         */
         $scope.toggleMenu = function () {
             $scope.showMenu = !$scope.showMenu;
-        }
+        };
 
+
+
+
+        /*
+         *Left hand side menu functions
+         */
+
+
+        /*
+         * Function which resets the @model, turning all items to done = false.
+         * @return {undefined}
+         */
         $scope.resetList = function () {
             // reset all the existing items in the model
             for (i = 0; i < $scope.model.length; i++) {
@@ -133,21 +193,27 @@ angular.module('myToDoApp', ['contenteditable', 'ngStorage', 'ngAnimate'])
             }
             // save change in local storage
             $localStorage.myToDos = $scope.model;
-            
+
             //toggle the menu
             $scope.toggleMenu();
 
-        }
-
+        };
+    
+    
+    
+        /*
+         * Function which deletes all items in the @model
+         * @return {undefined}
+         */
         $scope.deleteList = function () {
             // delete the entire list
             $scope.model = [];
-            
+
             // save change in local storage
             $localStorage.myToDos = $scope.model;
-            
+
             //toggle the menu
             $scope.toggleMenu();
-        }
+        };
 
     });
